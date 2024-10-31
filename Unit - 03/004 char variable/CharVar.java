99f1350b6e2a522a48dd09b71d385643658c61b1
dhruvil-84 <dhruvilvdhamecha84@gmail.com>
2024-10-31 22:19:43 +0530

    character variable program

---
+++ Unit - 03/004 char variable/CharVar.java
@@ -0,0 +1,13 @@
+class CharVar
+{
+    public static void main(String[] args)
+    {
+        char c1, c2;
+        c1 = 69;
+        c2 = 'm';
+        System.out.println("c1, c2 = " + c1 + ", " + c2);
+        c1++;
+        c2++;
+        System.out.println("c1, c2 = " + c1 + ", " + c2);
+    }
+}
