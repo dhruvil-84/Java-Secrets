c4880d4d7ccef59453a0698da629f409fd023ba4
dhruvil-84 <dhruvilvdhamecha84@gmail.com>
2024-10-31 22:21:17 +0530

    dynamic initialization of variables

---
+++ Unit - 03/006 Dynamic initialization/DynInit.java
@@ -0,0 +1,9 @@
+class DynInit
+{
+    public static void main(String[] args)
+    {
+        int a = 4, b = 3;
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("dynamically initialised c = " + c);
+    }
+}
